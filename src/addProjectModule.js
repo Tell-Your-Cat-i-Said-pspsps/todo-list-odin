a632085e73d0c9001315085f1bdf7d34074e5ab4
Ahmed El-Bayoumi <ahmedelbayoumi007@gmail.com>
2024-02-24 22:52:12 +0200

    remove unnecessary js modules

--- src/addProjectModule.js
+++
@@ -1,9 +0,0 @@
-export function addProjectModal() {
-  const addProjectModal = document.createElement("dialog");
-  addProjectModal.classList.add("addProjectModal");
-  const projectNameInputLabel = document.createElement("label");
-  projectNameInputLabel.setAttribute("for", "projectNameInput");
-  const projectNameInput = document.createElement("input");
-  projectNameInput.setAttribute("type", "text");
-  projectNameInput.setAttribute("id", "projectNameInput");
-}
